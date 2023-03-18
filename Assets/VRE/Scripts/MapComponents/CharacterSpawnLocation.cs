854496e9fb51c732989695db36df39568fae7e4a
lilhuskyvr <66710973+lilhuskyvr@users.noreply.github.com>
2023-03-18 18:57:27 +1100

    added gizmo (#8)
    added gizmo for player and character

--- Assets/VRE/Scripts/MapComponents/CharacterSpawnLocation.cs
+++ Assets/VRE/Scripts/MapComponents/CharacterSpawnLocation.cs
@@ -4,5 +4,10 @@ namespace VRE.Scripts.MapComponents
 {
     public class CharacterSpawnLocation : MonoBehaviour
     {
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(transform.position + Vector3.up, new Vector3(0.5f, 2, 0.5f));
+        }
     }
 }
